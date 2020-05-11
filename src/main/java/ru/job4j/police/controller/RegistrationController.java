bd8ff71e0f78424df808418ea171b2d803f15365
BonDyka <bondarew2507@gmail.com>
2020-05-11 19:47:28 +0300

    Replace spring-security on spring-boot-security

---
+++ src/main/java/ru/job4j/police/controller/RegistrationController.java
@@ -0,0 +1,9 @@
+package ru.job4j.police.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+}
---
+++ src/main/java/ru/job4j/police/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.job4j.police.domain;
+
+public enum  Role {
+    USER
+}
